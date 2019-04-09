6641c5a0bce20f5cc23d925ae7e31cc71753c889
wangjc <wangjc@cecurs.com>
2019-04-09 15:02:59 +0800

    Initial commit

---
+++ src/main/java/com/cecurs/qrfilemanger/QrfilemangerApplication.java
@@ -0,0 +1,13 @@
+package com.cecurs.qrfilemanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrfilemangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrfilemangerApplication.class, args);
+	}
+
+}
